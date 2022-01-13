dc1f2804446af09c10767bffe6f2056948c39077
Fadhl Aqbil Ramadhan <fadhl.hrs82@gmail.com>
2022-01-13 21:15:09 +0700

    added config and database from local

---
+++ database/migrations/2022_01_13_022840_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('total_amount');
+            $table->biginteger('total_price');
+            $table->biginteger('total_payment');
+            $table->biginteger('change');
+            $table->string('cashier_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
