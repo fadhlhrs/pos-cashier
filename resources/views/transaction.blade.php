0b532eb9c05dfefbf94fe5fca3fe01a635f13844
krnliaraisha <93108647+krnliaraisha@users.noreply.github.com>
2022-01-14 11:58:26 +0800

    Update transaction.blade.php

--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -1,13 +1,45 @@
 @extends('base')
 @section('content')
+
 <div class="container mt-5">
     <div class="row mb-4">
         <div class="col-12">
             <h5 class="mb-4">New Transaction</h5>
-            <div class="modal-body">
-                <div class="form-group">
-                    <input type="text" class="form-control" name="search" placeholder="Find Product">
-                </div>
+            <table class="table table-hover ">
+                <thead>
+                    <tr>
+                        <th scope="col ">Nama Produk</th>
+                        <th scope="col ">Harga</th>
+                        <th scope="col ">Jumlah</th>
+                        <th scope="col ">Status</th>
+                        <th scope="col ">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>L'oreal Shampoo 400ml</td>
+                        <td>62.000</td>
+                        <td>200pcs</td>
+                        <td>
+                            <span class="badge badge-info text-white ">Done</span>
+                            <span class="badge badge-danger text-white ">Pending</span>
+                        </td>
+                        <td>
+                            <div class="btn-group " role="group " aria-label="Basic example ">
+                                <a href="# " class="btn btn-primary text-white ">
+                                    <i class='bx bx-check'></i>
+                                </a>
+                                <a href="# " class="btn btn-info text-white ">
+                                    <i class='bx bx-pencil'></i>
+                                </a>
+                                <a href="# " class="btn btn-danger text-white ">
+                                    <i class='bx bx-trash'></i>
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
                 <div class="col-12">
                     <ul class="list-group product-list">
                         @foreach($inventories as $inventory)
